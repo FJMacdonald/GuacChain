dd2e72ca172f2ff83768dc6ae965aa5787f9a858
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-09-05 08:46:22 -0400

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by Francesca MACDONALD on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
